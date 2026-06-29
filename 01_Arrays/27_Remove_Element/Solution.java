b847854a4b1a96e1ce7b58f0bf3e8ddc48570ec9
Adnan Khan <tpadnankhan@gmail.com>
2026-06-29 20:21:04 +0530

    Refactor duplicate removal and add value removal method
    Refactored the removeDuplicates method and added removeElement method to remove specified value from array.

--- 01_Arrays/27_Remove_Element/Solution.java
+++ 01_Arrays/27_Remove_Element/Solution.java
@@ -1,30 +1,16 @@
-class Solution {
-    public int removeDuplicates(int[] nums) {
-        int l = 0;
-        int r = 1;
-        int j = 0;
-
-        int arr[] = new int[nums.length];
-
-        if (nums.length == 1)
-            return 1;
-
-        arr[j++] = nums[0];
-
-        while (r < nums.length) {
-            if (nums[l] != nums[r]) {
-                arr[j++] = nums[r++];
-                l++;
-            }
-            else {
-                l++;
-                r++;
-            }
+int removeElement(int* nums, int numsSize, int val) {
+    if (numsSize < 1)
+        return 0;
+    int nums1[numsSize];
+    int k = 0;
+    for (int i = 0; i < numsSize; i++) {
+        if (nums[i] != val) {
+            nums1[k] = nums[i];
+            k++;
         }
-
-        for (int i = 0; i < arr.length; i++)
-            nums[i] = arr[i];
-
-        return j;
     }
+    for (int i = 0; i < k; i++) {
+        nums[i] = nums1[i];
+    }
+    return k;
 }
